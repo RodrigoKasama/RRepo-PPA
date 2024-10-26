db39950370482ec426188d503afb7acf4a0467ad
LuquinhasPS <lucaspsilva_@hotmail.com>
2024-10-26 13:11:57 -0300

    delete

--- Script_Read.R
+++
@@ -1,9 +0,0 @@
-vinhos <- read.csv("vinhos.csv", header = FALSE, sep = ",")
-head(vinhos)
-
-media_vinhos <- colMeans(vinhos, na.rm = TRUE)
-
-desvio_padrao_vinhos <- sapply(vinhos, sd, na.rm = TRUE)
-
-media_vinhos
-desvio_padrao_vinhos
