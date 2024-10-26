446cf29f3bac4ceaefd1bad76eb286456103625f
LuquinhasPS <lucaspsilva_@hotmail.com>
2024-10-26 14:00:08 -0300

    Update 1C

--- Teste1/1C.R
+++ Teste1/1C.R
@@ -1,12 +1,21 @@
 library(ggplot2)
 library(dplyr)
 
+# Lendo o arquivo CSV sem cabeçalho
 vinhos <- read.csv("vinhos.csv", header = FALSE, sep = ",")
 
+# Definindo o nome da primeira coluna como tipo de vinho
 colnames(vinhos)[1] <- "Tipo"
 
+# Convertendo a coluna Tipo em um fator e definindo a ordem
 vinhos$Tipo <- factor(vinhos$Tipo, levels = sort(unique(vinhos$Tipo)))
 
+# Criando a pasta para salvar os gráficos, se não existir
+if (!dir.exists("graficos_1C")) {
+  dir.create("graficos_1C")
+}
+
+# Criando e salvando os gráficos de densidade
 for (i in 2:ncol(vinhos)) {
   p <- ggplot(vinhos, aes(x = vinhos[[i]], fill = Tipo)) +
     geom_density(alpha = 0.5) +
@@ -16,5 +25,7 @@ for (i in 2:ncol(vinhos)) {
     theme_minimal() +
     scale_fill_brewer(palette = "Set1")
   
+  # Salvando cada gráfico na pasta graficos_1C
   ggsave(filename = paste0("graficos_1C/densidade_", colnames(vinhos)[i], ".png"), plot = p)
 }
+
