2948af8994ea580fdd60b93f23655eccae006129
brunodutrademacedo <brunodemacedo.97@gmail.com>
2024-10-28 20:17:09 -0300

    Atualizando o codigo

--- Teste_n1/Q3/q3.R
+++ Teste_n1/Q3/q3.R
@@ -2,14 +2,18 @@ library(dplyr)
 library(tidyr)
 library(cluster)
 
+wine_dataset_path <- "Teste_n1/vinhos.csv"
+wine_data <- read.csv(wine_dataset_path, header = TRUE)
+colnames(wine_data)[1] <- "Class"
+
 # Lendo os dados
-wines <- read.table('http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', sep = ',', col.names = c('Type', 'Alcohol', 'Malic', 'Ash', 'Alcalinity', 'Magnesium', 'Phenols', 'Flavanoids', 'Nonflavanoids', 'Proanthocyanins', 'Color', 'Hue', 'Dilution', 'Proline'))
+wine_data <- read.table('http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', sep = ',', col.names = c('Type', 'Alcohol', 'Malic', 'Ash', 'Alcalinity', 'Magnesium', 'Phenols', 'Flavanoids', 'Nonflavanoids', 'Proanthocyanins', 'Color', 'Hue', 'Dilution', 'Proline'))
 
 # Ajustando o modelo de K-means
 modelo <- cluster_kmeans(k = 3)
 
 # Selecionando todas as colunas exceto "Type" para o modelo
-data_for_model <- select(wines, -Type)
+data_for_model <- select(wine_data, -Type)
 
 # Ajustando o modelo com todas as colunas exceto "Type"
 modelo <- fit(modelo, data_for_model)
@@ -25,17 +29,17 @@ calculate_entropy <- function(data) {
 
 # Calculando a entropia para cada grupo gerado pelo K-means
 entropy_clusters <- sapply(unique(clu), function(cluster_id) {
-  data_in_cluster <- wines$Type[clu == cluster_id]
+  data_in_cluster <- wine_data$Type[clu == cluster_id]
   calculate_entropy(data_in_cluster)
 })
 
 # Calculando a entropia para a classe "Type" original
-entropy_type <- calculate_entropy(wines$Type)
+entropy_type <- calculate_entropy(wine_data$Type)
 
 # Exibindo a entropia para cada grupo gerado pelo K-means
 print("Entropia para cada grupo gerado pelo K-means:")
 print(entropy_clusters)
 
 # Exibindo a entropia para a classe "Type" original
-print("Entropia para a classe 'Type' original:")
+print("Entropia para a classe original:")
 print(entropy_type)
