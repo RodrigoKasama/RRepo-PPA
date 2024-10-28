13e6e53ae7eb4a5c6369a023e260dd2ed70f32ac
RodrigoKasama <rodrigo.parracho@gmail.com>
2024-10-28 11:17:33 -0300

    Refactor da Q1

--- Teste_n1/Q1/q1_sketch.R
+++ Teste_n1/Q1/q1_sketch.R
@@ -13,51 +13,76 @@ stats <- t(data.frame(
 
 
 # Q1 - b
-dw_mean_grouped = wine_data %>%
+dw_mean_grouped <- wine_data %>%
   group_by(Class) %>%
-  summarise(across(.cols = everything(), .fns = mean))
+  summarise(across(.cols = everything(), .fns = mean, .names = "Mean_{.col}"))
 
-dw_sd_grouped = wine_data %>%
+dw_sd_grouped <- wine_data %>%
   group_by(Class) %>%
-  summarise(across(.cols = everything(), .fns = sd))
+  summarise(across(.cols = everything(), .fns = sd, .names = "SD-{.col}"))
 
+dw_stats_grouped <- t(left_join(dw_mean_grouped, dw_sd_grouped, by = "Class"))
+print(dw_stats_grouped)
+
+# Definindo parâmetros fixos
+class_label <- 'Class'
+colors <- c('#0099e5', '#34bf49', '#ff4c4c')
+alpha <- 0.7
 
 #Q1 - c
 
-plots = list()
-for(col_name in colnames(wine_data)[-1]) {
-  plots[[col_name]] = plot_density_class(wine_data %>% select(Class, col_name), class_label = 'Class', 
-                                         label_x = paste(col_name, 'by wine class'), 
-                                         colors = c('#0099e5', '#ff4c4c', '#34bf49'), 
-                                         alpha = .7)
-}
+plots <- list()
+# Iterando por cada coluna
+plots <- lapply(colnames(wine_data)[-1], function(col_name) {
+  plot_density_class(
+    wine_data[, c('Class', col_name), drop = FALSE], 
+    class_label = class_label, 
+    label_x = paste(col_name, 'by wine class'), 
+    colors = colors, 
+    alpha = alpha
+  )
+})
 
-density_plot_grouped = grid.arrange(grobs = plots, ncol = 5)
+# Gerando uma grid de graficos
+density_plot_grouped <- grid.arrange(grobs = plots, ncol = 5)
 
 # Q1 - d
 
-plots = list()
-for(col_name in colnames(wine_data)[-1]) {
-  plots[[col_name]] = plot_boxplot_class(wine_data %>% select(Class, col_name), 
-                                         class_label = 'Class', 
-                                         label_x = paste(col_name, 'by wine class'), 
-                                         colors = c('#0099e5', '#ff4c4c', '#34bf49'))
-}
+plots <- list()
+plots <- lapply(colnames(wine_data)[-1], function(col_name) {
+  plot_boxplot_class(
+    wine_data[, c('Class', col_name), drop = FALSE], 
+    class_label = class_label, 
+    label_x = paste(col_name, 'by wine class'), 
+    colors = colors
+  )
+})
 
-boxplot_grouped = grid.arrange(grobs = plots, ncol = 5)
+boxplot_grouped <- grid.arrange(grobs = plots, ncol = 5)
 
-#Q1 - e
-plots = list()
-len = 5
-attrs = colnames(wine_data)[-1]
-for(i in 1:len) {
-  for(j in 1:len) {
-    if(i < j) {
-      plots[[paste(attrs[i], attrs[j], sep = 'x')]] = plot_scatter(wine_data %>% select(x = attrs[i], value = attrs[j], variable = Class),
-                                                                   label_x = attrs[i], label_y = attrs[j], colors = c("red", "green", "blue"))
-    }
-  }
-}
+# Q1 - e
+plots <- list()
+# Obtenção das combinações de pares de colunas
+attrs <- colnames(wine_data)[-1]
+combs <- combn(attrs, 2, simplify = FALSE)
+max_plots <- 12
 
-scatter_plot_grouped = grid.arrange(grobs = plots, ncol = 5)
+combs <- combs[1:min(length(combs), max_plots)]
+
+# Gerando os gráficos com lapply
+plots <- lapply(combs, function(pair) {
+  plot_scatter(
+    wine_data %>% select(x = pair[1], value = pair[2], variable = Class),
+    label_x = pair[1],
+    label_y = pair[2],
+    colors = colors
+  )
+})
+
+# Nomeando os gráficos na lista com base nos pares de atributos
+names(plots) <- sapply(combs, function(pair) paste(pair, collapse = 'x'))
+
+# Organizando os gráficos em uma grade
+scatter_plot_grouped <- grid.arrange(grobs = plots, ncol = 5)
+
 
